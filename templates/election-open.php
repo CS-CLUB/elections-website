57a30b3d1a18873be54c01f67da8d9dfaddeec39
Jon <gsoc.student@gmail.com>
2012-08-05 01:31:05 -0400

    Added the election open and closed templates
    Added the election open and closed templates which are displayed
    for users who are not logged in.

---
+++ templates/election-open.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ *  UOIT/DC Computer Science Club Elections Website
+ *  Copyright (C) 2012 UOIT/DC Computer Science Club
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ * Default content to display when the election period is open, provides information
+ * about the Computer Science Club and links to register/website
+ */
+?>
+<div class="hero-unit">
+	<h1>Election Period Open</h1>
+	<br/>
+  <p> Thank you for visiting the Computer Science Club election website, the election period
+  		is <strong>currently open</strong>. Please login to vote for the executive positions.
+  		The election period is only open today and will be available for a full 24-hours from
+  		<strong>12:00am - 11:59pm</strong>, so unlike the nomination period it is important that 
+  		you <strong>login today</strong> and vote in the election.
+  </p>
+  <p>
+  		Election participation is only open to registered club members who have been a member of
+  		the Computer Science Club for at least one semester. If you would like to find out more about
+  		the Computer Science Club and how you can become a member, please click below to find out more.
+  </p>
+  <p><a href="http://uoitcsc.dyndns.org" class="btn btn-primary btn-large">Learn more »</a></p>
+</div>
