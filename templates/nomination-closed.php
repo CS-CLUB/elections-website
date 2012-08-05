2d368457055cd9623990caa89847e5a55d3f6543
Jon <gsoc.student@gmail.com>
2012-08-05 01:29:56 -0400

    Added the nomination open and closed templates
    Added the nomination open and closed templates which are displayed
    for users who are not logged in.

---
+++ templates/nomination-closed.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ *  UOIT/DC Computer Science Club Elections Website
+ *  Copyright (C) 2012 UOIT/DC Computer Science Club
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ * Default content to display when the nomination period is closed, some years this page may
+ * not apply as the election period will open within 1 minute of the nomination period closeing!
+ * Provides information about the Computer Science Club and links to register/website
+ */
+?>
+<div class="hero-unit">
+	<h1>Nomination Period Closed</h1>
+	<br/>
+  <p> Thank you for visiting the Computer Science Club election website, the nomination period
+  		is <strong>currently closed</strong>. The final election period for the executive positions 
+  		takes place on the first day after <strong>September 14</strong> that lands on a weekday. The 
+  		election period on that day is open for a full 24-hours from <strong>12:00am - 11:59pm</strong>.
+  		So unlike the nomination period it is important that you remember to vote on the day of the final election.
+  </p>
+  <p>
+  		Election participation is only open to registered club members who have been a member of
+  		the Computer Science Club for at least one semester. If you would like to find out more about
+  		the Computer Science Club and how you can become a member, please click below to find out more.
+  </p>
+  <p><a href="http://uoitcsc.dyndns.org" class="btn btn-primary btn-large">Learn more »</a></p>
+</div>
---
+++ templates/nomination-open.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ *  UOIT/DC Computer Science Club Elections Website
+ *  Copyright (C) 2012 UOIT/DC Computer Science Club
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ * Default content to display when the nomination period is open, provides information
+ * about the Computer Science Club and links to register/website
+ */
+?>
+<div class="hero-unit">
+	<h1>Nomination Period Open</h1>
+	<br/>
+  <p> Thank you for visiting the Computer Science Club election website, the nomination period
+  		is <strong>currently open</strong>. Please login if you would like to nominate yourself 
+  		for positions and to vote for nominees. The nomination period for candidates starts at the 
+  		beginning of each fall semester on <strong>September 1</strong> and is closed on 
+  		<strong>September 14</strong> at 11:59pm. The final election period for the executive positions
+  		takes place on the first day after <strong>September 14th</strong> that lands on a weekday from 
+  		12:00am - 11:59pm.
+  </p>
+  <p>
+  		Election participation is only open to registered club members who have been a member of
+  		the Computer Science Club for at least one semester. If you would like to find out more about
+  		the Computer Science Club and how you can become a member, please click below to find out more.
+  </p>
+  <p><a href="http://uoitcsc.dyndns.org" class="btn btn-primary btn-large">Learn more »</a></p>
+</div>
