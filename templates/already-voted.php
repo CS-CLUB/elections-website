0d830dbe06fd00606d49d9ae8a8ad82cc2e3a803
Jon <gsoc.student@gmail.com>
2012-08-08 23:05:07 -0400

    Added a template for users who have already voted

---
+++ templates/already-voted.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ *  UOIT/DC Computer Science Club Elections Website
+ *  Copyright (C) 2012 UOIT/DC Computer Science Club
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ * Default content to display when a user has already voted in either the nomination
+ * or election period. Thanks the user for voting and provides them with information
+ * about when the election results will be posted.
+ */
+?>
+<div class="alert alert-success">
+	<h1>Thank You for Voting!</h1>
+	<br/>
+  <p> 
+  		Thank You for Voting! Members like you are what keeps the Computer Science Club going
+  		strong, thank you for taking the time to vote in this years election, please be sure to
+  		visit the website after the voting period has ended to view the results.
+  </p>
+  <p>
+ 		The nomination period is open until <strong>September 14</strong> at 11:59pm, after which 
+ 		point the votes will be tallied and the final election will take place. The final election 
+ 		period for the executive positions takes place on the first day after <strong>September 14</strong>
+ 		that lands on a weekday. The election period on that day is open for a full 24-hours from 
+ 		12:00am - 11:59pm.
+  </p>
+</div>
