5caa4d8d8304dce6975f913f264fb8d02e0b2924
Jon <gsoc.student@gmail.com>
2012-08-09 23:21:56 -0400

    Added template for the final election voting form

---
+++ templates/election-form.php
@@ -0,0 +1,107 @@
+<?php
+/*
+ *  UOIT/DC Computer Science Club Elections Website
+ *  Copyright (C) 2012 UOIT/DC Computer Science Club
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+ * The final election voting form, displays a form with the list of candidates and
+ * incumbents for each position.
+ * 
+ * 
+ * DEPENDENCIES
+ * ------------
+ * 
+ * This template depends on a multi-dimensional array $nominees containing the positions
+ * and an array of the nominees for each position
+ * 
+ * An array mapping the positions to the incumbent
+ * $candidates = array(	'President'         => '',
+ *						'Vice President'    => '',
+ *						'Coordinator'       => '',
+ *						'Treasurer'         => ''
+ *					   );
+ *	
+ * An array mapping the positions to the incumbent
+ * $incumbents = array( 'President'         => '',
+ *						'Vice President'    => '',
+ *						'Coordinator'       => '',
+ *						'Treasurer'         => ''
+ *						);
+ */
+?>
+<section id="election-form">
+	<div class="page-header">
+		<h1>Election Period Voting</h1>
+	</div>
+	<br />
+	<form class="form-horizontal" action="index.php" method="post" accept-charset="UTF-8">
+		<fieldset>
+			<div class="control-group">
+	      		<label for="president_elect" class="control-label">President</label>
+		      	<div class="controls">
+		      		<select id="president_elect" class="input-xlarge">
+		      			<option></option>
+		      			<?php 
+      						echo '<option>' . $candidates['President'][0] . ' (Candidate)' . '</option>';
+      						echo '<option>' . $incumbents['President'][0] . ' (Incumbent)' . '</option>';
+		      			?>
+		      		</select>
+		      	</div>
+      		</div>
+      		<div class="control-group">
+	      		<label for="vicepresident_elect" class="control-label">Vice President</label>
+		      	<div class="controls">
+		      		<select id="vicepresident_elect" class="input-xlarge">
+		      			<option></option>
+		      			<?php 
+      						echo '<option>' . $candidates['Vice President'][0] . ' (Candidate)' . '</option>';
+      						echo '<option>' . $incumbents['Vice President'][0] . ' (Incumbent)' . '</option>';
+		      			?>
+		      		</select>
+		      	</div>
+      		</div>
+      		<div class="control-group">
+	      		<label for="coordinator_elect" class="control-label">Coordinator</label>
+		      	<div class="controls">
+		      		<select id="coordinator_elect" class="input-xlarge">
+		      			<option></option>
+		      			<?php 
+      						echo '<option>' . $candidates['Coordinator'][0] . ' (Candidate)' . '</option>';
+      						echo '<option>' . $incumbents['Coordinator'][0] . ' (Incumbent)' . '</option>';
+		      			?>
+		      		</select>
+		      	</div>
+      		</div>
+      					<div class="control-group">
+	      		<label for="treasurer_elect" class="control-label">Treasurer</label>
+		      	<div class="controls">
+		      		<select id="treasurer_elect" class="input-xlarge">
+		      			<option></option>
+		      			<?php 
+      						echo '<option>' . $candidates['Treasurer'][0] . ' (Candidate)' . '</option>';
+      						echo '<option>' . $incumbents['Treasurer'][0] . ' (Incumbent)' . '</option>';
+		      			?>
+		      		</select>
+		      	</div>
+      		</div>
+      		<div class="form-actions">
+            	<button class="btn btn-primary" type="submit" name="election_vote" value="Submit Vote">Submit Vote</button>
+          </div>
+		</fieldset>
+	</form>
+</section>
