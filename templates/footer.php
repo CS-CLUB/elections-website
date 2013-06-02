f1b4079e7ff6d919f23e0b9a0c02776b8e63d2e4
gnu_user <gsoc.student@gmail.com>
2013-06-02 16:39:22 -0400

    Fixed the pie chart tooltip issue -- had to use jquery 1.9.1
    The tooltip issue and other rendering issues with Highcharts were due
    to the fact that we were using the latest version of jquery (1.10.1)
    which Highcharts is not compatible with. The recommended version to use
    with Highcharts is jquery 1.9.1.

--- templates/footer.php
+++ templates/footer.php
@@ -39,7 +39,7 @@
     <!-- Javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery-1.10.1.min.js"></script>
+    <script src="js/jquery-1.9.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/highcharts/highcharts.min.js"></script>
     <script src="js/results.js"></script>
