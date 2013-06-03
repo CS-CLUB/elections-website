0af331d68dcc06e2c6897ab0781b4d12aecec42c
gnu_user <gsoc.student@gmail.com>
2013-06-03 01:37:36 -0400

    Added a method to determine when election is over

--- inc/election.php
+++ inc/election.php
@@ -71,7 +71,7 @@ function is_nomination($mysqli_elections)
  * 
  * @param mysqli $mysqli_elections The mysqli connection object for the ucsc elections DB
  * @return boolean True if the nomination period of the election is closed
-*/
+ */
 function is_nomination_closed($mysqli_elections)
 {
 	global $nomination_end_date, $election_start_date;
@@ -79,7 +79,7 @@ function is_nomination_closed($mysqli_elections)
 	$cur_date = DateTime::createFromFormat('Y-m-d-H-i', date('Y-m-d-H-i'));
 	
 	/* September 14th (11:59pm) */
-	$nomination_end = DateTime::createFromFormat('m-d-H-i', $nomination_end_date); 
+	$nomination_end = DateTime::createFromFormat('Y-m-d-H-i', date('Y').'-'.$nomination_end_date); 
 	
 	/* Election start day, first weekday after September 14 at 11:59pm */
 	$election_start = DateTime::createFromFormat('Y-m-d-H-i', $election_start_date);
@@ -87,7 +87,7 @@ function is_nomination_closed($mysqli_elections)
 	/*
 	 * If the current date is after the nomination period, before the election period,
 	 * and the election tables exist then the nomination period is closed
-	*/
+	 */
 	if ($cur_date > $nomination_end && $cur_date < $election_start
 		&& election_tables_exist($mysqli_elections))
 	{
@@ -126,6 +126,34 @@ function is_election($mysqli_elections)
 	*/
 	if ($cur_date >= $election_start && $cur_date <= $election_end
 			&& election_tables_exist($mysqli_elections))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * A function which determines if the election has finished and is closed for voting.
+ * @package election
+ * 
+ * @param mysqli $mysqli_elections The mysqli connection object for the ucsc elections DB
+ * @return boolean True if the election is open for voting
+ */
+function is_election_closed($mysqli_elections)
+{
+	global $nomination_end_date, $election_start_date, $election_end_date;
+	
+	$cur_date = DateTime::createFromFormat('Y-m-d-H-i', date('Y-m-d-H-i'));
+
+	/* End of the first week day (11:59pm) after September 14th */
+	$election_end = DateTime::createFromFormat('Y-m-d-H-i', $election_end_date);
+
+	/*
+	 * If the current date is within the nomination period, and the election tables exist
+	* then it is the nomination period
+	*/
+	if ($cur_date > $election_end && election_tables_exist($mysqli_elections))
 	{
 		return true;
 	}
