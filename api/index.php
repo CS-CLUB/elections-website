c19ee143a3b0dde9430af152fe2ade0d13e4c69c
gnu_user <gsoc.student@gmail.com>
2013-06-03 01:37:58 -0400

    Added security mechanism, cannot GET results until after election

--- api/index.php
+++ api/index.php
@@ -20,6 +20,7 @@
 */
 require_once '../inc/db_interface.php';
 require '../inc/election_auth.php';
+require_once '../inc/election.php';
 require 'Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 
@@ -44,6 +45,8 @@ $app->run();
  * Displays the results as a JSON encoded array of the positions and the 
  * names of the individuals and the number of votes they received.
  * 
+ * @package api
+ *
  * @param string $type The type of election period ("nomination" or "election")
  * @param string $position The position to get the results for (President, Vice President,
  * Coordinator, Treasurer). The default if no argument is provided is to get all positions.
@@ -61,18 +64,24 @@ function getResults($type, $position)
 		exit();
 	}
 
-	/* Encode the results as JSON */
-	echo json_encode(get_results($mysqli_elections, $type, $position));
+	/* Only get the results if the election period is closed */
+	if (is_election_closed($mysqli_elections))
+	{
+		/* Encode the results as JSON */
+		echo json_encode(get_results($mysqli_elections, $type, $position));
+	}
 }
 
 /* Get the results of a nomination or election period. Use this function
  * to get the vote breakdown after the nomination/election for ALL positions.
-*
-* Displays the results as a JSON encoded array of the positions and the
-* names of the individuals and the number of votes they received.
-*
-* @param string $type The type of election period ("nomination" or "election")
-*/
+ *
+ * Displays the results as a JSON encoded array of the positions and the
+ * names of the individuals and the number of votes they received.
+ *
+ * @package api
+ *
+ * @param string $type The type of election period ("nomination" or "election")
+ */
 function getResultsAll($type)
 {
 	global $db_user, $db_pass, $db_elec_name;
@@ -86,8 +95,12 @@ function getResultsAll($type)
 		exit();
 	}
 
-	/* Encode the results as JSON */
-	echo json_encode(get_results($mysqli_elections, $type));
+	/* Only get the results after the election period is closed */
+	if (is_election_closed($mysqli_elections))
+	{
+		/* Encode the results as JSON */
+		echo json_encode(get_results($mysqli_elections, $type));
+	}
 }
 
 ?>
